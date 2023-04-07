7746ecd0e48d2a79d066502a0d093c121d0c9b7b
Garry7777777 <garry4978@gmail.com>
2023-04-07 05:11:06 +0300

    init

---
+++ src/main/java/com/skypro/telematics/TelematicsApp.java
@@ -0,0 +1,13 @@
+package com.skypro.telematics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelematicsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelematicsApp.class, args);
+    }
+
+}
